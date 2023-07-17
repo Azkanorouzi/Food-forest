f2bb5c9525fc63817db5ed7a8f94f8f08a442a81
Azka norouzi <azka@azkas-MacBook-Air.local>
2023-07-17 13:47:50 +0330

    do

--- src/home/home.js
+++ src/home/home.js
@@ -1,6 +1,52 @@
-import imgUrl from './assets/pattern.png'
-const img = new Image()
-img.src = imgUrl
-img.classList.add('pattern')
-document.body.querySelector('.container').appendChild(img)
-console.log(container)
+import { Page } from '../general/general'
+//  Assets
+import patternUrl from './assets/pattern.png'
+import chefUrl from './assets/chef.png'
+import fullBurgerUrl from './assets/fullBurger.png'
+import upBurgerUrl from './assets/upburgerbun.png'
+import bottomBurgerUrl from './assets/bottomBurgerbun.png'
+import 'bootstrap-icons/bootstrap-icons.svg'
+class HomePage extends Page {
+  constructor() {
+    super()
+    this.name = 'home-page'
+  }
+  _generateHtml() {
+    return `
+    <div class="home-page__background parallax" data-speed="1"></div>
+    <main class="${this.name} page">
+        <h1 class='logo parallax' data-speed = "5">Food Forest</h1>
+        <img src="${patternUrl}" alt="" class="${this.name}__pattern parallax" data-speed="3">
+        <img src="${chefUrl}" alt="" class="${this.name}__chef-img parallax" data-speed="10">
+        <section class="nav">
+          <ul class="nav__list">
+            <li class="nav__link--menu nav__link"
+            data-link="menu"><svg class="bi" width="16" height="16">
+            <use xlink:href="#heart" />
+          </svg>Menu</li>
+            <li class="nav__link nav__link--home"data-link="home"><i class="bi bi-house"></i>Home</li>
+            <li class="nav__link nav__link--about" data-link="about"><i class="bi bi-info"></i>About</li>
+          </ul>
+          <img src="${fullBurgerUrl}" alt="" class=" parallax burger-menu--full burger" data-speed="3">
+          <img src="${upBurgerUrl}" alt="" class=" parallax burger-menu--up burger" data-speed="3">
+          <img src="${bottomBurgerUrl}" alt="" class=" parallax burger-menu--bottom burger" data-speed="3">
+        </section>
+    </main>
+    `
+  }
+  applyParallax(className) {
+    const parallax = document.addEventListener('mousemove', function (e) {
+      this.querySelectorAll('.parallax').forEach((layer) => {
+        const speed = layer.getAttribute('data-speed')
+        const x = (window.innerWidth - e.pageX * speed) / 100
+        const y = (window.innerHeight - e.pageY * speed) / 100
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+      })
+    })
+  }
+  _getBurger() {
+    return document.querySelector('.burger-menu--full')
+  }
+}
+const homePage = Object.deepFreeze(new HomePage())
+export default homePage
