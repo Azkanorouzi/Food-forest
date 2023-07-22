ca8b2f5be0cc7b64e587b19ef36ee51bfff2e9c0
Azka norouzi <azka@azkas-MacBook-Air.local>
2023-07-22 17:24:17 +0330

    Fix curPage bug

--- src/general/controlPages.js
+++ src/general/controlPages.js
@@ -4,9 +4,11 @@ import recipes from '../recipes/recipes'
 import '../recipes/recipes'
 
 export const controlPages = {
+  curPage: '',
   pages: { home, about, recipes },
   init() {
     home.render()
+    this.curPage = 'home'
   },
   start() {
     this.listenToLinks(document.querySelector('.nav__list'))
@@ -14,8 +16,13 @@ export const controlPages = {
   listenToLinks(links) {
     links.addEventListener('click', (e) => {
       const clickedLink = e.target.closest('.nav__link')
-      if (!e.target.closest('.nav__link')) return
+      if (
+        !e.target.closest('.nav__link') ||
+        this.curPage === clickedLink.dataset.link
+      )
+        return
       this.pages[clickedLink.dataset.link].render()
+      this.curPage = clickedLink.dataset.link
       this.start()
     })
   },
